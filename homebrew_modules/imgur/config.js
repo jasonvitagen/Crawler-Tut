f1176698ec8ed84f765f22b4e84fb5ab5f4b93ce
Jason Cheng <jasoncheng@live.com.my>
2014-12-02 23:21:28 +0800

    COMMIT 4

---
+++ homebrew_modules/imgur/config.js
@@ -0,0 +1,15 @@
+var config = {
+
+	imgur_apis : {
+		v3 : {
+			image_upload : {
+				method : 'post',
+				route : 'https://api.imgur.com/3/image'	
+			}
+		}
+	}
+
+};
+
+
+module.exports = config;
---
+++ homebrew_modules/imgur/imgur.js
@@ -0,0 +1,7 @@
+var config = require('./config');
+
+function Imgur () {
+	
+}
+
+module.exports = Imgur;
